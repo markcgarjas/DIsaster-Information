f15712e9f7e9db4d027a060a3c3b591a14583960
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-02 16:34:17 +0800

    done comment new and create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,33 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_login!, except: [:index]
+  before_action :authenticate_user!, except: [:index]
   before_action :set_post
   def index
     @comments = @post.comments
   end
 
-  
+
+  def new
+    @comment = @post.comments.build
+  end
+
+  def create
+    @comment = @post.comments.build(params_comment)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render :new
+    end
+  end
 
   private
-
 
   def set_post
     @post = Post.find(params[:post_id])
   end
+
+  def params_comment
+    params.require(:comment).permit(:content)
+  end
+
 end
