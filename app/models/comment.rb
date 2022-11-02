e74a3af942580ece8fffe6d2d13f504536a742b3
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-02 19:01:38 +0800

    add counter cache

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   validates :content, presence: true
   belongs_to :user
-  belongs_to :post
+  belongs_to :post,counter_cache: true
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_072719) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_101121) do
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.bigint "post_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_072719) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comments_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
