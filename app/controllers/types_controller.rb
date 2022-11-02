30e2bb259434ab57b8363d8b16ab82ddc575f1e2
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-02 14:38:23 +0800

    done new,create and update action. Add routes types

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,12 +1,49 @@
 class TypesController < ApplicationController
-
+  before_action :set_params_type, only: [:edit, :update]
   def index
     @types = Type.all
   end
 
+  def new
+    @type = Type.new
+  end
+
+  def create
+    @type = Type.new(params_type)
+    if @type.save
+      flash[:notice] = "Type of Disaster was created successfully."
+      redirect_to types_path
+    else
+      render :new
+    end
+  end
+
+  def edit ;
+
+  end
+
+  def show
+
+  end
+
+  def update
+    if @type.update(params_type)
+      flash[:notice] = "Category was updated successfully."
+      redirect_to types_path
+    else
+      render :edit
+    end
+  end
+
+
   private
 
-  def params_category
-    params.require(:category).permit(:name)
+  def params_type
+    params.require(:type).permit(:name)
   end
+
+  def set_params_type
+    @type = Type.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "posts#index"
 
+  resources :types
   resources :posts
 end
