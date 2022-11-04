48ab7f8c8eb53a0fd78478fbc39eb6df7ed4c317
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-04 14:10:02 +0800

    done "hot" tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
   before_action :set_post_params, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.includes(:user, :types).order(comments_count: :desc).kept
-    @hot_posts = Post.order(comments_count: :desc).select(:id).limit(3)
+    @hot_posts = Post.order(comments_count: :desc).limit(3).select{ |post| post.comments_count >= 1 }
+
   end
 
   def new
