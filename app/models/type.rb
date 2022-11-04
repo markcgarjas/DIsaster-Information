6b51dfd0029ecee239a016b597b24dd0ea9d0d52
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-04 18:30:49 +0800

    adjust design and add dependent: restriction with exception

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,7 +1,7 @@
 class Type < ApplicationRecord
   validates :name, presence: true
 
-  has_many :post_type_ships
+  has_many :post_type_ships, dependent: :restrict_with_exception
   has_many :posts, through: :post_type_ships
 
 
