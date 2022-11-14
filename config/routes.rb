c2f31a232a4fb15e260d9b3addaebc3b4bd82de3
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-14 16:39:52 +0800

    done api provinces

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get "/:unique_string", to: "posts#short_url"
 
   namespace :api do
-    resources :regions, only: :index, defaults: { format: :json }
+    resources :regions, only: :index, defaults: { format: :json } do
+      resources :provinces, only: :index, defaults: { format: :json }
+    end
   end
 end
