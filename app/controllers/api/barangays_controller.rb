52dfc7a6807a0309f6d8054312979781ba011ec7
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-15 17:20:52 +0800

    update barangays and city municipalities controller

--- app/controllers/api/barangays_controller.rb
+++ app/controllers/api/barangays_controller.rb
@@ -1,7 +1,7 @@
 class Api::BarangaysController < ApplicationController
 
   def index
-    city_municipality = Address::CityMunicipality.find(params[:city_municipality_id])
+    city_municipality = Address::CityMunicipality.find_by_id(params[:city_municipality_id])
     barangays = city_municipality.barangays
     render json: barangays, each_serializer: BarangaySerializer
   end
--- app/controllers/api/city_municipalities_controller.rb
+++ app/controllers/api/city_municipalities_controller.rb
@@ -1,9 +1,14 @@
 class Api::CityMunicipalitiesController < ApplicationController
 
   def index
-    district = Address::District.find(params[:district_id])
-    city_municipalities = district.city_municipalities
-    render json: city_municipalities, each_serializer: CityMunicipalitySerializer
+    province = Address::Province.find_by_id(params[:province_id])
+    district = Address::District.find_by_id(params[:district_id])
+    if province
+      city_municipalities = province.city_municipalities
+      render json: city_municipalities, each_serializer: CityMunicipalitySerializer
+    else
+      city_municipalities = district.city_municipalities
+      render json: city_municipalities, each_serializer: CityMunicipalitySerializer
+    end
   end
-
 end
