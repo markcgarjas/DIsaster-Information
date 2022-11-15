2fa476efca813d3db319865a3a13ee8787b1e3f7
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-15 17:21:12 +0800

    delete unnecessary controller and helpers

--- app/controllers/api/barangay_provinces_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::BarangayProvincesController < ApplicationController
-
-  def index
-    city_municipality = Address::CityMunicipality.find(params[:city_municipality_province_id])
-    barangays = city_municipality.barangays
-    render json: barangays, each_serializer: BarangaySerializer
-  end
-
-end
--- app/controllers/api/city_municipality_provinces_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::CityMunicipalityProvincesController < ApplicationController
-
-  def index
-    province = Address::Province.find(params[:province_id])
-    city_municipalities = province.city_municipalities
-    render json: city_municipalities, each_serializer: CityMunicipalitySerializer
-  end
-end
