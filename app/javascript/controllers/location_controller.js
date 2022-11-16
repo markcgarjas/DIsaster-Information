163aeba3fa079a8a7026f40bc9806151ec50a95f
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-16 14:17:59 +0800

    done city municipalities and barangays level hierarchy select for address

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -24,4 +24,44 @@ export default class extends Controller {
     })
   }
 
+  fetchCityMunicipalities() {
+      let target = this.selectCityMunicipalityIdTarget
+      $(target).empty();
+      $.ajax({
+        type: 'GET',
+        url: '/api/regions/' + this.selectRegionIdTarget.value + '/provinces/' + this.selectProvinceIdTarget.value + '/city_municipalities',
+        dataType: 'json',
+        success: (response) => {
+          $.each(response, function (index, record) {
+            console.log(record.name)
+            console.log(record.id)
+            let option = document.createElement('option')
+            option.value = record.id
+            option.text = record.name
+            target.appendChild(option)
+          })
+        }
+      })
+    }
+
+    fetchBarangays() {
+          let target = this.selectBarangayIdTarget
+          $(target).empty();
+          $.ajax({
+            type: 'GET',
+            url: '/api/regions/' + this.selectRegionIdTarget.value + '/provinces/' + this.selectProvinceIdTarget.value + '/city_municipalities/' + this.selectCityMunicipalityIdTarget.value + '/barangays',
+            dataType: 'json',
+            success: (response) => {
+              $.each(response, function (index, record) {
+                console.log(record.name)
+                console.log(record.id)
+                let option = document.createElement('option')
+                option.value = record.id
+                option.text = record.name
+                target.appendChild(option)
+              })
+            }
+          })
+        }
+
 }
