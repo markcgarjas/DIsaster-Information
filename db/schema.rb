7f4639ed91f18ea6652992c02f16682a5ef6ada2
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-17 16:09:39 +0800

    add column genre to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_062242) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_080144) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -88,12 +88,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_062242) do
     t.string "unique_string"
     t.string "ip_address"
     t.string "avatar"
+    t.bigint "address_district_id"
     t.bigint "address_region_id"
     t.bigint "address_province_id"
     t.bigint "address_city_municipality_id"
     t.bigint "address_barangay_id"
     t.index ["address_barangay_id"], name: "index_posts_on_address_barangay_id"
     t.index ["address_city_municipality_id"], name: "index_posts_on_address_city_municipality_id"
+    t.index ["address_district_id"], name: "index_posts_on_address_district_id"
     t.index ["address_province_id"], name: "index_posts_on_address_province_id"
     t.index ["address_region_id"], name: "index_posts_on_address_region_id"
     t.index ["discarded_at"], name: "index_posts_on_discarded_at"
@@ -119,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_062242) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
