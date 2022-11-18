e09dedcb2f67c725125eaa2c0f66866013860105
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-18 17:55:06 +0800

    fix typo and add back root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
 
   constraints(ClientDomainConstraint.new) do
     resources :posts do
