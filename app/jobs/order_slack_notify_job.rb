45633084b09007d63d1807daa1ad1f17f4732c6a
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-25 13:42:01 +0800

    update slack notifier

--- app/jobs/order_slack_notify_job.rb
+++ app/jobs/order_slack_notify_job.rb
@@ -3,7 +3,7 @@ class OrderSlackNotifyJob < ApplicationJob
 
   def perform(order_id)
     order = Order.find order_id
-    notifier = Slack::Notifier.new 'https://hooks.slack.com/services/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'
-    notifier.ping "user: #{order.user.email}, place order amount #{order.amount}, serial number: #{order.serial_number}"
+    notifier = Slack::Notifier.new 'https://hooks.slack.com/services/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'
+    notifier.ping "The user #{order.user.email} is order!\nThe amount is: #{order.amount}, \nThe serial number is: #{order.serial_number}, \nThe status is: #{order.state}"
   end
 end
